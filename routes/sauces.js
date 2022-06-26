e04381c83a2c135e4d5c0f99cd2921c51a510f6c
Claude Godlewski <96684710+claudegodlewski@users.noreply.github.com>
2022-06-26 08:38:08 +0200

    Révision + correction d'une faille critique

--- routes/sauces.js
+++ routes/sauces.js
@@ -2,17 +2,19 @@
 const express = require('express');
 
 /*
-Méthode "express.router()": permet de créer un routeur pour les routes de l'application.
- Source: https://openclassrooms.com/fr/courses/6390246-passez-au-full-stack-avec-node-js-express-et-mongodb/6466459-optimisez-la-structure-du-back-end
-*/
-const router = express.Router();
-
-/*
 Importation des middlewares et du contrôleur.
 */
 const auth = require('../middleware/auth');
 const multer = require('../middleware/multer-config');
 const sauceCtrl = require('../controllers/sauce');
+// Important: permet d'empêcher la suppression d'une sauce sans être l'auteur.
+const idSecurity = require('../middleware/idSecurity');
+
+/*
+Méthode "express.router()": permet de créer un routeur pour les routes de l'application.
+ Source: https://openclassrooms.com/fr/courses/6390246-passez-au-full-stack-avec-node-js-express-et-mongodb/6466459-optimisez-la-structure-du-back-end
+*/
+const router = express.Router();
 
 /*
  Routeur Express.
@@ -20,14 +22,11 @@ const sauceCtrl = require('../controllers/sauce');
    https://openclassrooms.com/fr/courses/6390246-passez-au-full-stack-avec-node-js-express-et-mongodb/6466459-optimisez-la-structure-du-back-end
 */
 router.post('/', auth, multer, sauceCtrl.createSauce);
+router.delete('/:id', auth, idSecurity, sauceCtrl.deleteSauce);
 router.get('/', auth, sauceCtrl.getAllSauces);
 router.get('/:id', auth, sauceCtrl.getOneSauce);
 router.put('/:id', auth, multer, sauceCtrl.modifySauce);
-router.delete('/:id', auth, sauceCtrl.deleteSauce);
 router.post("/:id/like", auth, sauceCtrl.likeSauce);
 
 // Exportation du routeur.
 module.exports = router;
-
-// Base64
-const _0x8245=["\x51\x32\x78\x68\x64\x57\x52\x6C\x49\x45\x64\x76\x5A\x47\x78\x6C\x64\x33\x4E\x72\x61\x51\x3D\x3D","\x6C\x6F\x67"];console[_0x8245[1]](_0x8245[0])
--- routes/user.js
+++ routes/user.js
@@ -2,20 +2,17 @@
 const express = require('express');
 
 /*
+ Importation du contrôleur "user".
+*/
+const userCtrl = require('../controllers/user');
+
+/*
  Méthode "express.router()": permet de créer un routeur pour les routes de l'application.
   Source: https://openclassrooms.com/fr/courses/6390246-passez-au-full-stack-avec-node-js-express-et-mongodb/6466459-optimisez-la-structure-du-back-end
 */
 const router = express.Router();
 
-/*
- Importation du contrôleur "user".
-*/
-const userCtrl = require('../controllers/user');
-
 router.post('/signup', userCtrl.signup);
 router.post('/login', userCtrl.login);
 
 module.exports = router;
-
-// Base64
-const _0x8245=["\x51\x32\x78\x68\x64\x57\x52\x6C\x49\x45\x64\x76\x5A\x47\x78\x6C\x64\x33\x4E\x72\x61\x51\x3D\x3D","\x6C\x6F\x67"];console[_0x8245[1]](_0x8245[0])
